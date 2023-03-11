5be21f940e19b46ccc100d92994899b589b6ef88
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-11 17:45:12 +0300

    [FIXES] Illegal char <:> at index 40: com.recognize.app-mergeDebugResources-33:/values/values.xml)

--- app/src/main/java/com/steve_md/mpesa_daraja_sdk/interceptor/AccessTokenInterceptor.kt
+++ app/src/main/java/com/steve_md/mpesa_daraja_sdk/interceptor/AccessTokenInterceptor.kt
@@ -1,4 +1,19 @@
 package com.steve_md.mpesa_daraja_sdk.interceptor
 
-class AccessTokenInterceptor {
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+
+/**
+ *
+ * This class encode the Consumer Key and Secret to
+ * create the auth token and pass it as a header for our JSON request to the API
+ *
+ */
+class AccessTokenInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+                TODO()
+        }
+
 }
